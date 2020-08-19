0dd6f6a4850c2f8dd4dcae6ff38b6773658f01c8
stukene <estuken53@gmail.com>
2020-08-19 15:56:55 -0700

    tested

--- scraper.py
+++ scraper.py
@@ -4,16 +4,19 @@ from bs4 import BeautifulSoup
 class Scraper:
 
 
-        def scrape(url, html_elm, id_or_class, tag): 
-            page = requests.get(url)
-            soup = BeautifulSoup(page.content, 'html.parser')
-            job_elems
-            if(tag == 'id'):
-                job_elems = soup.find(id=id_or_class)
-            else:
-                job_elems = soup.find("div", class_=id_or_class)
+    def scrape(url, html_elm, id_or_class, tag): 
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        job_elems = 0
+        if(tag == 'id'):
+            job_elems = soup.find(id=id_or_class)
+        if(tag == 'class'):
+            job_elems = soup.find(html_elm, class_=id_or_class)
 
 
-    
-            print(job_elems.prettify())
-            return job_elems  
+
+        print(job_elems.prettify())
+        return job_elems
+    # for testing
+    URL = 'https://lyrics.fandom.com/wiki/Bob_Dylan:The_Times_They_Are_A-Changin\''
+    scrape(URL, "div", "lyricbox", 'class')

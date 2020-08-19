b4b4dbb7e050e82904ee3aa3bd02822637f1560e
stukene <estuken53@gmail.com>
2020-08-19 15:51:29 -0700

    Made Scrape function

--- scraper.py
+++ scraper.py
@@ -1,2 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
+
+class Scraper:
+
+
+        def scrape(url, html_elm, id_or_class, tag): 
+            page = requests.get(url)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            job_elems
+            if(tag == 'id'):
+                job_elems = soup.find(id=id_or_class)
+            else:
+
+
+
+            job_elems = soup.find("div", class_='lyricbox')
+    
+            print(job_elems.prettify())
+            return job_elems  
